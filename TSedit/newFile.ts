1d7249f77d9808bd46904cc3e44260e6d6eab673
yuan29r <sheng29r@gmail.com>
2019-12-13 14:46:42 +0800

    protected and private練習

--- TSedit/newFile.ts
+++ TSedit/newFile.ts
@@ -44,7 +44,7 @@ var cs: Clock2Static = Clock2;
 var newClock = new cs(7, 30);
 /****************************************** */
 
-/**介面擴充 */
+/**介面擴充***********************************/
 interface Shape {
     color: string;
 }
@@ -68,3 +68,81 @@ var square2 = <Square2>{};
 square2.color = "blue";
 square2.sideLength = 10;
 square2.penWidth = 5.0;
+
+interface Counter{
+    (start:number):string;
+    interval:number;
+    reset():void;
+}
+
+var c:Counter;
+c(10);
+c.reset();
+c.interval=5;
+
+//**************************************下面就像在寫C#和JAVA */
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+var greeter = new Greeter("world");
+
+/******************************************繼承 */
+class Animal {
+    name: string;
+    constructor(theName: string) { this.name = theName; }
+    move(distanceInMeters: number = 0) {
+        alert(`${this.name} moved ${distanceInMeters}m.`);
+    }
+}
+
+class Snake extends Animal {
+    constructor(name: string) { super(name); }
+    move(distanceInMeters = 5) {
+        alert("Slithering...");
+        super.move(distanceInMeters);
+    }
+}
+
+class Horse extends Animal {
+    constructor(name: string) { super(name); }
+    move(distanceInMeters = 45) {
+        alert("Galloping...");
+        super.move(distanceInMeters);
+    }
+}
+
+var sam = new Snake("Sammy the Python");
+var tom: Animal = new Horse("Tommy the Palomino");
+
+sam.move();
+tom.move(34);
+
+/*****************************************************protected */
+class Person {
+    protected name: string;
+    constructor(name: string) { this.name = name; }
+}
+
+class Employee extends Person {
+    private department: string;
+    
+    constructor(name: string, department: string) {
+        super(name);
+        this.department = department;
+    }
+    //這邊還是可以使用protected宣告的變數喔
+    public getElevatorPitch() {
+        return `Hello, my name is ${this.name} and I work in ${this.department}.`;
+    }
+}
+
+var howard = new Employee("Howard", "Sales");
+console.log(howard.getElevatorPitch());
+
--- TSoutput/newFile.js
+++ TSoutput/newFile.js
@@ -29,4 +29,59 @@ var square2 = {};
 square2.color = "blue";
 square2.sideLength = 10;
 square2.penWidth = 5.0;
+var c;
+c(10);
+c.reset();
+c.interval = 5;
+//**************************************下面就像在寫C#和JAVA */
+class Greeter {
+    constructor(message) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+var greeter = new Greeter("world");
+/******************************************繼承 */
+class Animal {
+    constructor(theName) { this.name = theName; }
+    move(distanceInMeters = 0) {
+        alert(`${this.name} moved ${distanceInMeters}m.`);
+    }
+}
+class Snake extends Animal {
+    constructor(name) { super(name); }
+    move(distanceInMeters = 5) {
+        alert("Slithering...");
+        super.move(distanceInMeters);
+    }
+}
+class Horse extends Animal {
+    constructor(name) { super(name); }
+    move(distanceInMeters = 45) {
+        alert("Galloping...");
+        super.move(distanceInMeters);
+    }
+}
+var sam = new Snake("Sammy the Python");
+var tom = new Horse("Tommy the Palomino");
+sam.move();
+tom.move(34);
+/*****************************************************protected */
+class Person {
+    constructor(name) { this.name = name; }
+}
+class Employee extends Person {
+    constructor(name, department) {
+        super(name);
+        this.department = department;
+    }
+    //這邊還是可以使用protected宣告的變數喔
+    getElevatorPitch() {
+        return `Hello, my name is ${this.name} and I work in ${this.department}.`;
+    }
+}
+var howard = new Employee("Howard", "Sales");
+console.log(howard.getElevatorPitch());
 //# sourceMappingURL=newFile.js.map
